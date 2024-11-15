920730a2131c34489a43a9f83ae6a7ebcf1ac1df
ChristianEMV <20193tn065@utez.edu.mx>
2024-11-15 16:27:59 -0600

    [ADD] Items Vida & Mana

---
+++ Assets/Scripts/Inventario/ItemPocionMana.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Items/PocionMana")]
+public class ItemPocionMana : InventarioItem
+{
+    [Header("Posicion info")]
+    public float MPrestauracion;
+}
---
+++ Assets/Scripts/Inventario/ItemPocionVida.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Items/PocionVida")]
+public class ItemPocionVida: InventarioItem
+{
+    [Header("Pocion info")]
+    public float HPrestuaracion;
+
+}
